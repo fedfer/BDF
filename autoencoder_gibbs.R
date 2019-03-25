789ec13963780be07c936775f6830b10a9a25ac5
fedfer <ff31@duke.edu>
2019-03-25 16:18:21 -0400

    try constraints

--- autoencoder_gibbs.R
+++ autoencoder_gibbs.R
@@ -2,9 +2,9 @@
 library(bayesSurv)
 
 ### Generate Data
-p = 20;k = 5; n = 200
+p = 20;k = 12; n = 700
 L = matrix(rnorm(p*k,0,1),p,k)
-X = matrix(rnorm(n*k,0,1),n,k)%*%t(L)
+X = scale(matrix(rnorm(n*k,0,1),n,k)%*%t(L))
 #X = matrix(rnorm(n*p,0,1),n,p)
 
 beta_true = numeric(p);beta_true[c(1,2,3)] = c(1,-1,1)
@@ -48,7 +48,8 @@ for(s in 1:nrun){
    V = solve(diag(csi) + theta%*%t(theta)/sigmasq)
    for(i in 1:n){
       
-      m = V%*%(theta*y[i]/sigmasq + t(Lambda)%*%diag(psi)%*%X[i,])
+      #m = V%*%(theta*y[i]/sigmasq + t(Lambda)%*%diag(psi)%*%X[i,])
+      m = V%*%(theta*y[i]/sigmasq + diag(csi)%*%Gamma%*%X[i,])
       eta[i,] = bayesSurv::rMVNorm(1, 
                                mean = m, 
                                Sigma = V)
@@ -61,19 +62,22 @@ for(s in 1:nrun){
    }
    
    # --- update Lambda 
-   eta2 = t(eta) %*% eta    # prepare eta crossproduct before the loop
-
-   for(j in 1:p) {
-      
-      V = solve(eta2*psi[j] + diag(k))
-      m = psi[j]*V%*%t(eta)%*%X[,j]
-      Lambda[j,] = bayesSurv::rMVNorm(1, 
-                                      mean = m, 
-                                      Sigma = V)
-   }
+   # No constraint
+   #eta2 = t(eta) %*% eta    # prepare eta crossproduct before the loop
+   #for(j in 1:p) {
+   #   
+   #   V = solve(eta2*psi[j] + diag(k))
+   #   m = psi[j]*V%*%t(eta)%*%X[,j]
+   #   Lambda[j,] = bayesSurv::rMVNorm(1, 
+   #                                   mean = m, 
+   #                                   Sigma = V)
+   #}
+   
+   # Constraint
+   Lambda = Psi%*%t(Gamma)%*%diag(csi)
    
    # --- update Gamma ---
-   
+   # No constraint
    for(j in 1:k) {
       
       V = solve(X2*csi[j] + diag(p))
@@ -83,6 +87,9 @@ for(s in 1:nrun){
                                       Sigma = V)
    }
 
+   # Constraint
+   #Gamma = Csi%*%t(Lambda)%*%diag(psi)
+   
    # --- update Psi ---
    X_til = X - eta %*% t(Lambda)
    psi = rgamma(p, as + 0.5*n, bs+0.5*colSums(X_til^2))
@@ -90,12 +97,17 @@ for(s in 1:nrun){
    
    
    # --- update Csi ---
+   # No constraint
    eta_til = eta - X %*% t(Gamma)
    csi = rgamma(k, as + 0.5*n, bs+0.5*colSums(eta_til^2))
    Csi = diag(1/csi)
    
+   # Constraint 
+   #Csi = solve(Gamma%*%diag(psi)%*%Lambda + 0.001*diag(k))
+   #csi = 1/diag(Csi)
+   
    # --- update theta ---
-   Vtheta = solve(t(eta)%*%eta/sigmasq + diag(rep(1,k))/100)
+   Vtheta = solve(eta2/sigmasq + diag(rep(1,k))/100)
    Mtheta = Vtheta%*%t(eta)%*%y/sigmasq
    theta = as.numeric(bayesSurv::rMVNorm(1,Mtheta,Vtheta))
    
@@ -110,12 +122,17 @@ for(s in 1:nrun){
       Gamma_st[s-burn,,] = Gamma
       Psi_st[s-burn,] = psi
       Csi_st[s-burn,] = csi
+      #beta[s-burn,] = as.numeric(t(theta)%*%Gamma)
       beta[s-burn,] = as.numeric(t(theta)%*%Gamma)
       sigmasq_st[s-burn] = sigmasq
    }
    
    if(s%%200 == 0){
       print(s)
+      print(mean((diag(psi)%*%Lambda - t(Gamma)%*%diag(csi))^2))
+      print(mean((diag(psi)%*%Lambda - t(Gamma)%*%diag(csi))))
+      print(diag((diag(psi)%*%Lambda - t(Gamma)%*%diag(csi))))
+      
    }
 
    
